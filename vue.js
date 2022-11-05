559f591be55d0044422742cfb00914c3f2a179d0
Krazick02 <elangelcotaymas@gmail.com>
2022-11-04 23:09:39 -0700

    Encriptacion con tres tipos

--- vue.js
+++ vue.js
@@ -36,6 +36,23 @@ const myApp = createApp({
                 ['Y', 25],
                 ['Z', 26]
             ],
+            abc8: [
+                ['A', 0],
+                ['B', 1],
+                ['C', 2],
+                ['D', 3],
+                ['W', 4],
+                ['X', 5],
+                ['Y', 6],
+                ['Z', 7]
+            ],
+            aei: [
+                ['A', 0],
+                ['E', 1],
+                ['I', 2],
+                ['O', 3],
+                ['U', 4]
+            ],
             resultado: '',
             clave: '',
             ingresado: [],
@@ -47,7 +64,8 @@ const myApp = createApp({
             entrada: '',
             url: '',
             regex: /^[a-zA-ZñÑ]*$/,
-            funcion: 'encriptar'
+            funcion: 'encriptar',
+            metodo: '27'
 
         }
     },
@@ -56,14 +74,17 @@ const myApp = createApp({
             this.clave = ''
             this.entrada = ''
             this.resultado = ''
-            this.clave = ''
             this.ingresado = []
             this.ingresadoClave = []
             this.encriptado = []
             this.cifrado = ''
-            this.entrada = ''
         },
-        ver() {
+        ver27() {
+            this.resultado = ''
+            this.ingresado = []
+            this.ingresadoClave = []
+            this.encriptado = []
+            this.cifrado = ''
             var ent = this.clave.length
             var text = this.clave
             for (var x = this.clave.length; x < this.entrada.length; x++) {
@@ -116,26 +137,145 @@ const myApp = createApp({
             console.log(this.ingresado)
             console.log(this.ingresadoClave)
         },
+        ver8() {
+            this.resultado = ''
+            this.ingresado = []
+            this.ingresadoClave = []
+            this.encriptado = []
+            this.cifrado = ''
+            var ent = this.clave.length
+            var text = this.clave
+            for (var x = this.clave.length; x < this.entrada.length; x++) {
+                for (var y = 0; y < ent; y++) {
+                    this.clave += text[y]
+                    if (this.entrada.length == this.clave.length) {
+                        break
+                    }
+                }
+                if (this.entrada.length == this.clave.length) {
+                    break
+                }
+            }
+
+            for (var x = 0; x < this.entrada.length; x++) {
+                for (var y = 0; y < 8; y++) {
+                    if (this.abc8[y][0] == this.entrada[x].toUpperCase()) {
+                        this.ingresado[this.ingresado.length] = (this.abc8[y][1])
+                    }
+                }
+            }
+            for (var x = 0; x < this.clave.length; x++) {
+                for (var y = 0; y < 8; y++) {
+                    if (this.abc8[y][0] == this.clave[x].toUpperCase()) {
+                        this.ingresadoClave[this.ingresadoClave.length] = (this.abc8[y][1])
+                    }
+                }
+            }
+
+            for (var x = 0; x < this.ingresado.length; x++) {
+                var contador = 0
+                for (var y = 0; y < this.abc8.length; y++) {
+                    if (((this.ingresado[x] + this.ingresadoClave[x]) % 8) == this.abc8[y][1]) {
+                        this.encriptado[this.encriptado.length] = this.abc8[y][0]
+                        break
+                    }
+                    contador++
+                }
+                console.log(contador)
+                this.resultado += this.abc8[contador][0]
+                contador = 0
+            }
+
+
+
+
+            for (var x = 0; x < this.encriptado.length; x++) {
+
+            }
+            console.log(this.ingresado)
+            console.log(this.ingresadoClave)
+        },
+        ver5() {
+            this.resultado = ''
+            this.ingresado = []
+            this.ingresadoClave = []
+            this.encriptado = []
+            this.cifrado = ''
+            var ent = this.clave.length
+            var text = this.clave
+            for (var x = this.clave.length; x < this.entrada.length; x++) {
+                for (var y = 0; y < ent; y++) {
+                    this.clave += text[y]
+                    if (this.entrada.length == this.clave.length) {
+                        break
+                    }
+                }
+                if (this.entrada.length == this.clave.length) {
+                    break
+                }
+            }
+
+            for (var x = 0; x < this.entrada.length; x++) {
+                for (var y = 0; y < 5; y++) {
+                    if (this.aei[y][0] == this.entrada[x].toUpperCase()) {
+                        this.ingresado[this.ingresado.length] = (this.aei[y][1])
+                    }
+                }
+            }
+            for (var x = 0; x < this.clave.length; x++) {
+                for (var y = 0; y < 5; y++) {
+                    if (this.aei[y][0] == this.clave[x].toUpperCase()) {
+                        this.ingresadoClave[this.ingresadoClave.length] = (this.aei[y][1])
+                    }
+                }
+            }
+
+            for (var x = 0; x < this.ingresado.length; x++) {
+                var contador = 0
+                for (var y = 0; y < this.aei.length; y++) {
+                    if (((this.ingresado[x] + this.ingresadoClave[x]) % 5) == this.aei[y][1]) {
+                        this.encriptado[this.encriptado.length] = this.aei[y][0]
+                        break
+                    }
+                    contador++
+                }
+                console.log(contador)
+                this.resultado += this.aei[contador][0]
+                contador = 0
+            }
+
+
+
+
+            for (var x = 0; x < this.encriptado.length; x++) {
+
+            }
+            console.log(this.ingresado)
+            console.log(this.ingresadoClave)
+        },
         let27() {
+            this.metodo = '27'
             this.regex = /^[a-zA-ZñÑ]*$/
             this.clean()
         },
         let8() {
+            this.metodo = '8'
             this.regex = /^[abcdwxyzABCDWXYZ]*$/
             this.clean()
         },
         let5() {
+            this.metodo = '5'
             this.regex = /^[aeiouAEIOU]*$/
             this.clean()
         },
         validar() {
-            if (!this.regex.test(this.entrada) || !this.regex.test(this.clave)) {
-                Swal.fire(
-                    'Error!',
-                    'No puedes ingresar numeros o simbolos :c!',
-                    'warning'
-                )
-            }
+            // if (!this.regex.test(this.entrada) || !this.regex.test(this.clave)) {
+            //     Swal.fire(
+            //         'Error!',
+            //         'No puedes ingresar numeros o simbolos :c!',
+            //         'warning'
+            //     )
+            // }
         }
 
 
